bb385016db873d68d74758baa35084885c06b04d
valentina <vs4153588@gmail.com>
2024-09-17 09:34:50 -0500

    Proceso 2

---
+++ Backend_Proyect_SENA/src/index.js
@@ -0,0 +1,11 @@
+//ESTO NO DEBE TOCARSE, YA ESTA FUNCIONAL
+import app from "./app.js";
+import { config } from "dotenv";
+import "./conexion.js";
+config();
+
+const { PORT } = process.env;
+const port = PORT || 9400;
+
+//puerto que estan ubicados
+app.listen(port, () => console.log(`App run on port ${port}`));
