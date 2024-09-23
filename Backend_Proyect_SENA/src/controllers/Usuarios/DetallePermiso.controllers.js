c5a34d5b548f5a3b77085a18d958ea499740283a
valentina <vs4153588@gmail.com>
2024-09-23 12:51:18 -0500

    MODIFICACION

--- Backend_Proyect_SENA/src/controllers/Usuarios/DetallePermiso.controllers.js
+++ Backend_Proyect_SENA/src/controllers/Usuarios/DetallePermiso.controllers.js
@@ -1,22 +1,27 @@
 
 export const asignarPermiso = async (req, res)=>{
-   /*  try {
-        const { UsuarioId, PermisoId } = req.body;
-        const UsuarioId = await Usuario.findByPk(usuarioId);
-        const permiso = await Permiso.findByPk(permisoId);
-  //jose quieto
-        if (!usuario || !permiso) {
-          return res.status(404).json({ message: 'Usuario o Permiso no encontrado' });
-        }
-  
-        // Añadir el permiso al usuario en la tabla intermedia
-        await UsuarioPermiso.create({ UsuarioId: usuario.id, PermisoId: permiso.id });
-  
-        res.status(200).json({ message: 'Permiso asignado al usuario exitosamente' });
-      } catch (error) {
-        res.status(500).json({ message: 'Error al asignar permiso al usuario', error });
-      } */
-}
+  try {
+    const {UsuarioId, PermisoId}= req.body;
+    const usuario = await Usuario.findById(UsuarioId);
+    const permiso = await Permiso.findById(PermisoId);
+ 
+    if(!usuario ||!permiso){
+        return res.status(404).json({msg: 'Usuario o permiso no encontrado'});
+    }
+ 
+    if(usuario.permisos.includes(permiso._id)){
+        return res.status(400).json({msg: 'El usuario ya tiene este permiso'});
+    }
+ 
+    await DetallePermiso.create({UsuarioId: usuario.id, PermisoId: permiso.id});
+    
+    res.status(200).json({ message: 'Permiso asignado al usuario exitosamente' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al asignar permiso al usuario', error });
+    }
+  }
+
+
 
 export const getPermisos = async (req, res)=>{
     
--- Backend_Proyect_SENA/src/models/Rol.js
+++ Backend_Proyect_SENA/src/models/Rol.js
@@ -4,6 +4,7 @@ import { conexion } from "../conexion.js";
 const Rol = conexion.define(
   "Rol",
   {
+    // ID automatico
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -13,51 +14,27 @@ const Rol = conexion.define(
     rolName: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
-      validate: {
-        notEmpty: {
-          msg: "El valor no puede estar vacio",
-        },
-      },
     },
   },
   {
     tableName: "Roles",
     timestamps: false,
-    indexes: [
-      {
-        unique: true,
-        fields: ["rolName"],
-      },
-    ],
-    hooks: {
-      beforeSave: (rol) => {
-        rol.rolName = rol.rolName.toLowerCase();
-      },
-    },
   }
 );
 
 const insertarRoles = async (datos) => {
   try {
     await Rol.sync();
+    const respuesta = await Rol.findAll();
 
-    for (const rol of datos) {
-      const [created] = await Rol.findOrCreate({
-        where: { rolName: rol.rolName.toLowerCase() },
-        defaults: { ...rol, rolName: rol.rolName.toLowerCase() },
-      });
-
-      if (!created) {
-        console.log(`El rol ${rol.rolName} ya existe.`);
-      }
+    if (respuesta.length === 0) {
+      await Rol.bulkCreate(datos);
     }
   } catch (error) {
-    console.error("Error al insertar roles:", error.message);
     throw new Error(error.message);
   }
 };
 
-insertarRoles([{ rolName: "ADMIN" }, { rolName: "USUARIO" }]);
+insertarRoles([{ rolName: "ADMIN" }, { rolName: "USUARIO" }, {rolName: "COORDINADOR"}]);
 
 export default Rol;
