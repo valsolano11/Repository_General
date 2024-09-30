8718c51356fc6ba520504947aa103ed03707c77f
valentina <vs4153588@gmail.com>
2024-09-30 13:22:08 -0500

    Get relaciones

--- Backend_Proyect_SENA/src/controllers/Formacion/Relacion.controllers.js
+++ Backend_Proyect_SENA/src/controllers/Formacion/Relacion.controllers.js
@@ -1,6 +1,43 @@
 import Instructores from "../../models/Instructores.js";
 import Fichas from "../../models/Fichas.js";
 import InstructorFicha from "../../models/FI_IN.js";
+import Usuario from "../../models/Usuario.js";
+
+export const obtenerRelaciones = async (req, res) => {
+  try {
+
+    const relaciones = await InstructorFicha.findAll({
+      include: [
+        {
+          model: Instructores,
+          attributes: ["nombre", "correo", "celular"],
+        },
+        {
+          model: Fichas,
+          attributes: ["NumeroFicha", "Programa", "Jornada"],
+        },
+        {
+          model: Usuario,
+          attributes: ["nombre", "correo"], // Datos del usuario que realizó la acción
+        },
+      ],
+    });
+
+    if (relaciones.length === 0) {
+      return res.status(404).json({
+          message: "No se encontraron relaciones entre instructores y fichas",
+        });
+    }
+
+    res.status(200).json(relaciones);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+        message: "Error al obtener las relaciones entre instructores y fichas",
+      });
+  }
+};
+
 
 export const asignarInstructorAFichas = async (req, res) => {
   try {
--- Backend_Proyect_SENA/src/routes/Relacion.routes.js
+++ Backend_Proyect_SENA/src/routes/Relacion.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { asignarInstructorAFichas } from "../controllers/Formacion/Relacion.controllers.js";
+import { asignarInstructorAFichas, obtenerRelaciones } from "../controllers/Formacion/Relacion.controllers.js";
 import { rutaProtegida } from "../middlewares/ValidarToken.js";
 
 const Relacion = express.Router();
 
 
 Relacion.post("/asignar-fichas",rutaProtegida, asignarInstructorAFichas);
+Relacion.get("/obtener-relaciones", rutaProtegida, obtenerRelaciones);
+
 
 export default Relacion;
