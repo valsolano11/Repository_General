e5507d0f1ed1e5ff78cd2c2fcebf0664c54d42d4
Ximena Limas <laura.limas0207@gmail.com>
2024-10-04 05:06:47 -0500

    get

--- Backend_Proyect_SENA/src/routes/Pedidos.routes.js
+++ Backend_Proyect_SENA/src/routes/Pedidos.routes.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import upload from '../middlewares/UploadPePres.js'; // Ajusta la ruta según tu estructura de carpetas
-import { actualizarPedido, crearPedido } from '../controllers/Productos/Pedido.controllers.js';
-import { actualizarSalidaProducto } from "../controllers/Coordinación/Salida.controller.js"; // Ajusta la ruta según la ubicación de tu controlador
+import upload from '../middlewares/UploadPePres.js'; 
+import { 
+  actualizarPedido, 
+  crearPedido, 
+  getPedido, 
+  getAllPedidos 
+} from '../controllers/Productos/Pedido.controllers.js';
+import { actualizarSalidaProducto } from "../controllers/Coordinación/Salida.controller.js"; 
 
 const router = express.Router();
 
+router.get('/pedido', getAllPedidos);
+router.get('/pedido/:id', getPedido);
 router.post('/pedido', crearPedido);
 router.put('/pedido/:id', upload.single('firma'), actualizarPedido);
 router.put("/pedido/:id/salida", actualizarSalidaProducto);
